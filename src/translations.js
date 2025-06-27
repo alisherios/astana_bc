2ffa81692b9c1a50ef4205ef45258e1298e6788e
alisherios <125376347+alisherios@users.noreply.github.com>
2025-06-27 11:19:08 +0500

    Add files via upload

---
+++ src/translations.js
@@ -0,0 +1,120 @@
+// Translations for the application
+export const translations = {
+  ru: {
+    // Navigation
+    mapTitle: "Карта Бизнес-Центров Астаны",
+    mapDescription: "Интерактивная карта с информацией о компаниях и услугах Казахтелеком",
+    mapButton: "Карта",
+    analyticsButton: "Аналитика",
+    
+    // Map Controls
+    allFilter: "Все",
+    ktFilter: "КТ",
+    nonKtFilter: "не КТ",
+    showHeatmap: "Показать тепловую карту",
+    hideHeatmap: "Скрыть тепловую карту",
+    separateMarkers: "Отдельные маркеры",
+    clustering: "Кластеризация",
+    selectZone: "Выделить зону",
+    cancelSelection: "Отменить выделение",
+    clearZone: "Очистить зону",
+    
+    // Zone Statistics
+    zoneStats: "Статистика по зоне",
+    zoneStatsDescription: "Данные по выделенной области",
+    businessCenters: "Бизнес-центров:",
+    companies: "Компаний:",
+    ktClients: "КТ клиентов:",
+    totalRevenue: "Общий доход:",
+    
+    // Organization Card
+    bin: "БИН:",
+    address: "Адрес:",
+    accruals: "Начисления:",
+    ktServices: "Услуги Казахтелеком:",
+    
+    // Business Center Card
+    totalCompanies: "Всего компаний:",
+    ktClientsCard: "КТ клиенты:",
+    otherCompanies: "Другие компании:",
+    
+    // Analytics Page
+    analyticsTitle: "Аналитика Бизнес-Центров Астаны",
+    analyticsDescription: "Детальная статистика по бизнес-центрам, компаниям и клиентам Казахтелеком",
+    allDistricts: "Все районы",
+    allTypes: "Все типы",
+    exportCsv: "Экспорт CSV",
+    distributionByDistricts: "Распределение по районам",
+    distributionDescription: "Количество бизнес-центров и компаний по районам",
+    buildingTypes: "Типы зданий",
+    buildingTypesDescription: "Распределение по назначению зданий",
+    businessCenter: "Бизнес-центр:",
+    
+    // Currency
+    currency: "тг"
+  },
+  
+  kk: {
+    // Navigation
+    mapTitle: "Астана Бизнес-Орталықтарының Картасы",
+    mapDescription: "Компаниялар мен Қазақтелеком қызметтері туралы ақпаратты қамтитын интерактивті карта",
+    mapButton: "Карта",
+    analyticsButton: "Аналитика",
+    
+    // Map Controls
+    allFilter: "Барлығы",
+    ktFilter: "ҚТ",
+    nonKtFilter: "ҚТ емес",
+    showHeatmap: "Жылу картасын көрсету",
+    hideHeatmap: "Жылу картасын жасыру",
+    separateMarkers: "Жеке белгілер",
+    clustering: "Кластерлеу",
+    selectZone: "Аймақты бөлу",
+    cancelSelection: "Таңдауды болдырмау",
+    clearZone: "Аймақты тазалау",
+    
+    // Zone Statistics
+    zoneStats: "Аймақ бойынша статистика",
+    zoneStatsDescription: "Бөлінген аумақ бойынша деректер",
+    businessCenters: "Бизнес-орталықтар:",
+    companies: "Компаниялар:",
+    ktClients: "ҚТ клиенттері:",
+    totalRevenue: "Жалпы кіріс:",
+    
+    // Organization Card
+    bin: "БСН:",
+    address: "Мекенжай:",
+    accruals: "Есептеулер:",
+    ktServices: "Қазақтелеком қызметтері:",
+    
+    // Business Center Card
+    totalCompanies: "Барлық компаниялар:",
+    ktClientsCard: "ҚТ клиенттері:",
+    otherCompanies: "Басқа компаниялар:",
+    
+    // Analytics Page
+    analyticsTitle: "Астана Бизнес-Орталықтарының Аналитикасы",
+    analyticsDescription: "Бизнес-орталықтар, компаниялар және Қазақтелеком клиенттері бойынша толық статистика",
+    allDistricts: "Барлық аудандар",
+    allTypes: "Барлық түрлер",
+    exportCsv: "CSV экспорты",
+    distributionByDistricts: "Аудандар бойынша бөлу",
+    distributionDescription: "Аудандар бойынша бизнес-орталықтар мен компаниялар саны",
+    buildingTypes: "Ғимарат түрлері",
+    buildingTypesDescription: "Ғимараттардың мақсаты бойынша бөлу",
+    businessCenter: "Бизнес-орталық:",
+    
+    // Currency
+    currency: "тг"
+  }
+};
+
+// Hook for using translations
+export const useTranslation = (language = 'ru') => {
+  const t = (key) => {
+    return translations[language]?.[key] || translations.ru[key] || key;
+  };
+  
+  return { t };
+};
+
